1f746865ebffc386b73c985cbfdf239797d51ff0
Adrian Carriger <mail@adrianandnicole.com>
2016-12-18 20:46:26 -0800

    fix(RemapPipe): use number sort instead of string

--- src/app/packages/filter/remap.pipe.ts
+++ src/app/packages/filter/remap.pipe.ts
@@ -15,6 +15,6 @@ export class RemapPipe implements PipeTransform {
    */
   transform(value: any, mapTo = 'default'): any {
     if (value === null || value === undefined) { return; }
-    return value.sort((a, b) => a.order[mapTo] > b.order[mapTo]);
+    return value.sort((a, b) => a.order[mapTo] - b.order[mapTo]);
   }
 }
