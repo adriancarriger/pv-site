0f9eb05a2a90727efcfac8688229362fed8c0dc9
Adrian Carriger <mail@adrianandnicole.com>
2017-01-30 22:12:37 -0800

    how did i miss that?

--- src/app/packages/filter/filter.pipe.spec.ts
+++ src/app/packages/filter/filter.pipe.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { Component, Injectable, NgModule, OnInit } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Subject } from 'rxjs/subject';
+import { Subject } from 'rxjs/Subject';
 
 import { FilterPipe } from './filter.pipe';
 import { FilterUtilitiesService } from './filter-utilities.service';
