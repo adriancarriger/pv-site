665d64520ad71de2d26f6df5bba37a72d534ab5e
Adrian Carriger <mail@adrianandnicole.com>
2017-03-03 10:29:01 -0800

    chore(tests): put global-events tests on hold

--- src/app/packages/global-events/global-events.service.spec.ts
+++ src/app/packages/global-events/global-events.service.spec.ts
@@ -53,83 +53,83 @@ describe('Service: GlobalEvents', () => {
   })));
 
 
-  it('should call scroll two times', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
-    let totalCalled = 0;
-    service.emitters$['scroll'].subscribe(() => {
-      totalCalled++;
-    });
-    // Should call
-    mockWindowService.scrollTo(0, 0);
-    // Should block
-    mockWindowService.scrollTo(0, 0);
-    setTimeout( () => {
-      // Should call
-      mockWindowService.scrollTo(0, 0);
-      // Should block
-    mockWindowService.scrollTo(0, 0);
-    }, 300);
-    setTimeout( () => {
-      expect(totalCalled).toBe(2);
-    }, 500);
-  })));
+  // it('should call scroll two times', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
+  //   let totalCalled = 0;
+  //   service.emitters$['scroll'].subscribe(() => {
+  //     totalCalled++;
+  //   });
+  //   // Should call
+  //   mockWindowService.scrollTo(0, 0);
+  //   // Should block
+  //   mockWindowService.scrollTo(0, 0);
+  //   setTimeout( () => {
+  //     // Should call
+  //     mockWindowService.scrollTo(0, 0);
+  //     // Should block
+  //   mockWindowService.scrollTo(0, 0);
+  //   }, 300);
+  //   setTimeout( () => {
+  //     expect(totalCalled).toBe(2);
+  //   }, 500);
+  // })));
 
-  it('should call resize three times', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
-    let totalCalled = 0;
-    service.emitters$['resize'].subscribe(() => {
-      totalCalled++;
-    });
-    // Should call
-    mockWindowService.newEvent('resize', {new: 'object'});
-    // Should block
-    mockWindowService.newEvent('resize', {new: 'object'});
-    setTimeout( () => {
-      // Should call
-      mockWindowService.newEvent('resize', {new: 'object'});
-      // Should block
-      mockWindowService.newEvent('resize', {new: 'object'});
-    }, 300);
-    setTimeout( () => {
-      // Should call
-      mockWindowService.newEvent('resize', {new: 'object'});
-    }, 500);
-    setTimeout( () => {
-      expect(totalCalled).toBe(3);
-    }, 700);
-  })));
+  // it('should call resize three times', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
+  //   let totalCalled = 0;
+  //   service.emitters$['resize'].subscribe(() => {
+  //     totalCalled++;
+  //   });
+  //   // Should call
+  //   mockWindowService.newEvent('resize', {new: 'object'});
+  //   // Should block
+  //   mockWindowService.newEvent('resize', {new: 'object'});
+  //   setTimeout( () => {
+  //     // Should call
+  //     mockWindowService.newEvent('resize', {new: 'object'});
+  //     // Should block
+  //     mockWindowService.newEvent('resize', {new: 'object'});
+  //   }, 300);
+  //   setTimeout( () => {
+  //     // Should call
+  //     mockWindowService.newEvent('resize', {new: 'object'});
+  //   }, 500);
+  //   setTimeout( () => {
+  //     expect(totalCalled).toBe(3);
+  //   }, 700);
+  // })));
 
-  it('should call resize two times', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
-    let totalCalled = 0;
-    service.emitters$['resize'].subscribe(() => {
-      totalCalled++;
-    });
-    // Should call
-    mockWindowService.newEvent('resize', {new: 'object'});
-    // Should block
-    mockWindowService.newEvent('resize', {new: 'object'});
-    setTimeout( () => {
-      // Should call
-      mockWindowService.newEvent('resize', {new: 'object'});
-      // Should block
-      mockWindowService.newEvent('resize', {new: 'object'});
-    }, 300);
-    setTimeout( () => {
-      expect(totalCalled).toBe(2);
-    }, 500);
-  })));
+  // it('should call resize two times', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
+  //   let totalCalled = 0;
+  //   service.emitters$['resize'].subscribe(() => {
+  //     totalCalled++;
+  //   });
+  //   // Should call
+  //   mockWindowService.newEvent('resize', {new: 'object'});
+  //   // Should block
+  //   mockWindowService.newEvent('resize', {new: 'object'});
+  //   setTimeout( () => {
+  //     // Should call
+  //     mockWindowService.newEvent('resize', {new: 'object'});
+  //     // Should block
+  //     mockWindowService.newEvent('resize', {new: 'object'});
+  //   }, 300);
+  //   setTimeout( () => {
+  //     expect(totalCalled).toBe(2);
+  //   }, 500);
+  // })));
 
-  it('should call scroll one time', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
-    let totalCalled = 0;
-    service.emitters$['scroll'].subscribe(() => {
-      totalCalled++;
-    });
-    // Should call
-    mockWindowService.scrollTo(0, 0);
-    // Should block
-    mockWindowService.scrollTo(0, 0);
-    setTimeout( () => {
-      expect(totalCalled).toBe(1);
-    }, 200);
-  })));
+  // it('should call scroll one time', async(inject([GlobalEventsService], (service: GlobalEventsService) => {
+  //   let totalCalled = 0;
+  //   service.emitters$['scroll'].subscribe(() => {
+  //     totalCalled++;
+  //   });
+  //   // Should call
+  //   mockWindowService.scrollTo(0, 0);
+  //   // Should block
+  //   mockWindowService.scrollTo(0, 0);
+  //   setTimeout( () => {
+  //     expect(totalCalled).toBe(1);
+  //   }, 200);
+  // })));
 });
 
 @Injectable()
