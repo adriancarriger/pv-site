0275a00dfd0a245b97510e06c8dd1c0443d29783
Adrian Carriger <mail@adrianandnicole.com>
2017-03-09 10:43:39 -0800

    test(sermons): use correct imports

--- src/app/shared/sermons-list/sermons-list.component.spec.ts
+++ src/app/shared/sermons-list/sermons-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MomentModule } from 'angular2-moment';
 
 import { SermonsListComponent } from './sermons-list.component';
 
@@ -8,6 +9,7 @@ describe('SermonsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ MomentModule ],
       declarations: [ SermonsListComponent ]
     })
     .compileComponents();
