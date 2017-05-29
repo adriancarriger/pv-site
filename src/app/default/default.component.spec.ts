ae08560b3c902d6f231af64316b47784ec4a2de8
adriancarriger <hello@adriancarriger.com>
2017-05-29 14:25:30 -0500

    test(sermons): add tests for last commit

--- src/app/default/default.component.spec.ts
+++ src/app/default/default.component.spec.ts
@@ -2,16 +2,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import {ActivatedRoute, Route } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DefaultComponent } from './default.component';
+import { FilterModule } from '../packages/filter/filter.module';
+import { SharedModule } from '../shared/shared.module';
+import { ApiService } from '../core/api/api.service';
+import { MockApiService } from '../core/media/media.service.spec';
+import { MediaService } from '../core/media/media.service';
 
 describe('DefaultComponent', () => {
   let component: DefaultComponent;
   let fixture: ComponentFixture<DefaultComponent>;
+  let mockApiService: MockApiService;
+  const config: Route[] = [
+    { path: '', component: DefaultComponent }
+  ];
 
   beforeEach(async(() => {
+    mockApiService = new MockApiService();
     TestBed.configureTestingModule({
-      declarations: [ DefaultComponent ]
+      imports: [
+        FilterModule.forRoot(),
+        RouterTestingModule.withRoutes(config),
+        SharedModule
+      ],
+      declarations: [ DefaultComponent ],
+      providers: [
+        MediaService,
+        { provide: ApiService, useValue: mockApiService },
+      ]
     })
     .compileComponents();
   }));
