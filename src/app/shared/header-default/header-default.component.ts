f13bdcd044dc6fc897c67ff9ffee7a7b08a6d1e5
adriancarriger <hello@adriancarriger.com>
2017-06-05 19:57:17 -0500

    fix(header): increase height with links

--- src/app/shared/header-default/header-default.component.ts
+++ src/app/shared/header-default/header-default.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header-default',
   templateUrl: './header-default.component.html',
   styleUrls: ['./header-default.component.scss']
 })
-export class HeaderDefaultComponent {
+export class HeaderDefaultComponent implements OnChanges {
   @Input() info;
   @Input() slug;
+  @HostBinding('class.has-links') hasLinks = false;
+
+  ngOnChanges() {
+    this.hasLinks = this.info && this.info.links;
+  }
 
   scrollTo(slug, index, linkTo) {
     const element = document.querySelector(`#${slug}-${linkTo ? linkTo : index}`);
