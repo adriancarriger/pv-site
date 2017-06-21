624a668d536b4480faa984663dc9b733f51c35c5
adriancarriger <hello@adriancarriger.com>
2017-06-21 07:10:33 -0500

    test(home): update tests based on last commit

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,7 +2,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -11,6 +13,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, SharedModule ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
