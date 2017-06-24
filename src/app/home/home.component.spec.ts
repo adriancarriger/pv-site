1bf6eb6ad9766ce914a4e0cb821f54e2f34e7c36
adriancarriger <hello@adriancarriger.com>
2017-06-24 07:17:47 -0500

    test(home): add tests for recent changes

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,20 +1,27 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, Injectable } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Subject } from 'rxjs/Subject';
 
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
+// import { MockApiService } from '../front-end/nav/nav.component.spec';
+import { ApiService } from '../core/api/api.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let mockApiService: MockApiService;
 
   beforeEach(async(() => {
+    mockApiService = new MockApiService();
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule, SharedModule ],
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: ApiService, useValue: mockApiService }
+      ]
     })
     .compileComponents();
   }));
@@ -29,3 +36,13 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Injectable()
+export class MockApiService extends ApiService {
+  constructor() {
+    super(null);
+  }
+  onInit() {
+    this.menu = <any>new Subject();
+  }
+}
