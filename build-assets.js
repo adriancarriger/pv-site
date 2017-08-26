9b67e2a4d42fe9c4995de3e7684b3ecd135e97c0
adriancarriger <hello@adriancarriger.com>
2017-08-26 16:51:52 -0500

    refactor(assets): improve image asset script

--- build-assets.js
+++ build-assets.js
@@ -2,11 +2,15 @@ const Jimp = require('jimp');
 const glob = require('glob')
 const fs = require('fs-extra');
 
-fs.removeSync('./src/assets/images/');
-glob('./src/assets/*.jpg', {}, (err, files) => {
+const imageBase = './src/assets'
+const imageDistBase = `${imageBase}/images/`;
+
+fs.removeSync(imageDistBase);
+glob(`${imageBase}/*.{jpg,jpeg,png}`, {}, (err, files) => {
   if (err) throw err;
   console.log('🔨 - Building assets...');
   files.forEach(file => {
+    const fileBase = file.split(/\.|\//).reverse();
     Jimp.read(file, (error, image) => {
       if (error) throw error;
       [
@@ -14,19 +18,10 @@ glob('./src/assets/*.jpg', {}, (err, files) => {
         { name: 'medium', w: 800, h: 200 },
         { name: 'large', w: 1000, h: 300 }
       ].forEach(item => {
-        const fileName = file
-          .split('/')
-          .reverse()[0]
-          .split('.jpg')
-          .join(`-${item.name}.jpg`);
-        const writeLocation = file
-          .split('assets/')
-          .join('assets/images/')
-          .split('.jpg')
-          .join(`-${item.name}.jpg`);
+        const fileName = `${fileBase[1]}-${item.name}.${fileBase[0]}`;
         image.cover(item.w, item.h)
           .quality(80)
-          .write(writeLocation);
+          .write(`${imageDistBase}${fileName}`);
         console.log(`✅ - ${fileName}`)
       });
     });
