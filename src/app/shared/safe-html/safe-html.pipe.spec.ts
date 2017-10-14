20ff75ea79314264b8a1f187f41dbbcc9cc17c2c
adriancarriger <hello@adriancarriger.com>
2017-10-14 18:18:28 -0500

    test(safe-html): increase test coverage

--- src/app/shared/safe-html/safe-html.pipe.spec.ts
+++ src/app/shared/safe-html/safe-html.pipe.spec.ts
@@ -2,7 +2,15 @@ import { SafeHtmlPipe } from './safe-html.pipe';
 
 describe('SafePipe', () => {
   it('create an instance', () => {
-    // const pipe = new SafeHtmlPipe();
-    // expect(pipe).toBeTruthy();
+    const mockDomSanitizer = new MockDomSanitizer();
+    const pipe = new SafeHtmlPipe(<any>mockDomSanitizer);
+    const result = pipe.transform('my-string');
+    expect(result).toBe('my-string-my-string');
   });
 });
+
+export class MockDomSanitizer {
+  bypassSecurityTrustHtml(value) {
+    return `${value}-${value}`;
+  }
+}
