59b8abb9bb5a5c596ff06efa49195e477b38d994
adriancarriger <hello@adriancarriger.com>
2017-10-29 07:07:51 -0500

    fix(contact): use FormData object as body

--- src/app/core/legacy-api/legacy-api.service.ts
+++ src/app/core/legacy-api/legacy-api.service.ts
@@ -7,15 +7,13 @@ export class LegacyApiService {
   constructor(private httpClient: HttpClient) { }
 
   submitContactMessage(input) {
-    console.log(input);
-    const postData = {
-      'full-name': input.name,
-      'contact-email': input.email,
-      'contact-message': input.message
-    };
-    this.httpClient.post('https://api.pvbiblechurch.com/ajax-contact-form/', postData).subscribe(output => {
+    const url = 'https://api.pvbiblechurch.com/ajax-contact-form/';
+    const postData = new FormData();
+    postData.append('full-name', input.name);
+    postData.append('contact-email', input.email);
+    postData.append('contact-message', input.message);
+    this.httpClient.post(url, postData).subscribe(output => {
       console.log('Done!', output);
     });
   }
-
 }
